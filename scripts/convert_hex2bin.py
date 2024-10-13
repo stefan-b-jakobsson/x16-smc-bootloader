5df851642f46e25cae8b064bcee52eed6ef97e1c
stefan-b-jakobsson <stefan.b.jakobsson@gmail.com>
2024-10-13 19:14:39 +0300

    Don't allow the bootloader to start with 0x8a

--- scripts/convert_hex2bin.py
+++ scripts/convert_hex2bin.py
@@ -1,5 +1,14 @@
 from intelhex import IntelHex
 
+# Load HEX file
 bootloader = IntelHex()
 bootloader.fromfile("build/bootloader.hex", format="hex")
+bootloader[0] = 0x8a
+
+# Validate start of bootloader != 0x8a
+if bootloader[0] == 0x8a:
+    print("Error: Bootloader may not start with byte value 0x8a")
+    exit(1)
+
+# Create bin file
 bootloader.tofile("build/bootloader.bin", format="bin")
