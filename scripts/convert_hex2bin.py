f424029f6b3557517ce706d09591404b2836812b
stefan-b-jakobsson <stefan.b.jakobsson@gmail.com>
2024-10-13 22:56:53 +0300

    Clean up code comments

--- scripts/convert_hex2bin.py
+++ scripts/convert_hex2bin.py
@@ -6,8 +6,15 @@ bootloader.fromfile("build/bootloader.hex", format="hex")
 bootloader[0] = 0x8a
 
 # Validate start of bootloader != 0x8a
+#   Before bootloader v3, 0x1e00 was set to the magic value 0x8a to indicate that
+#   a bootloader was present. From bootloader v3 the magic value and version
+#   number has been moved to the end of flash memory (0x1ffe-0x1fff).
+#   The start of the bootloader is expected to be a table of rjmp instructions.
+#   There is one rjmp instruction with a target address within the bootloader area
+#   where the low byte is 0x8a. This rjmp instruction should consequently be avoided.
+#   This script exits with an error code to remind us about the above.
 if bootloader[0] == 0x8a:
-    print("Error: Bootloader may not start with byte value 0x8a")
+    print("ERROR: The bootloader may not start with byte value 0x8a.")
     exit(1)
 
 # Create bin file
